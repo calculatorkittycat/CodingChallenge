0ac918028a35c564555d74d1acec8a92c6fd5186
Oleg Iakim <execute067@gmail.com>
2021-07-20 00:19:26 -0700

    Final commit

--- src/main/java/EvenNumbers.java
+++ src/main/java/EvenNumbers.java
@@ -1,17 +1,24 @@
 /*
+Even Numbers
 
+Even number is an integer (positive or negative, not a fraction, including 0), which is divisible by 2.
 
+Example: -6, -2, 0, 8, 20, 100
+
+Problem: Print out even numbers up to given number: 10
  */
-
-
-
 
 package main.java;
 
 public class EvenNumbers {
 
     public static void main(String[] args) {
-        System.out.println(" ");
-
+        int max = 10;
+        System.out.print("Even numbers are: ");
+        for (int i = 0; i <= max; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + ", ");
+            }
+        }
     }
 }
