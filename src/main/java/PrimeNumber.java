f9840f23769a4e64e7703b637ec1544fbb2ab405
Oleg Iakim <execute067@gmail.com>
2021-07-21 15:20:21 -0700

    First commit

--- src/main/java/PrimeNumber.java
+++ src/main/java/PrimeNumber.java
@@ -12,5 +12,21 @@ package main.java;
 
 public class PrimeNumber {
 
+    public static boolean isPrime(int number) {
 
+        if (number <= 1) { return false; } // Negative and 1 are not Prime
+        if (number == 2) { return true; }   // 2 is only Even prime number
+        if (number % 2 == 0) { return false; } // Excluding all Even numbers
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) { return false; }}
+            return true;
+    }
+
+    public static void main(String[] args) {
+
+        int max = 100;
+        for (int i = 1; i <= max; i++) {
+            if (isPrime(i)) { System.out.print(i + ", "); }
+        }
+    }
 }
