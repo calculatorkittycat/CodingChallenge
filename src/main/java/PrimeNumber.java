d09897399b4b271dbfbc8b3e8df5f157e5ab1764
Oleg Iakim <execute067@gmail.com>
2021-07-21 15:12:23 -0700

    First commit

---
+++ src/main/java/PrimeNumber.java
@@ -0,0 +1,16 @@
+/*
+Prime Numbers
+
+Prime number is a number (positive only) which only divisible by 1 and itself.
+
+Example: 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31
+
+Problem: Print out prime numbers up to given number: 100
+ */
+
+package main.java;
+
+public class PrimeNumber {
+
+
+}
