a3449887fb2edd1e9ade8c3f047097c15b37caaa
Oleg Iakim <execute067@gmail.com>
2021-07-22 16:09:31 -0700

    First commit

---
+++ src/main/java/PerfectNumbers.java
@@ -0,0 +1,19 @@
+/*
+Perfect Numbers
+
+Perfect number is a positive integer that is equal to the sum of its proper divisors (6 => 1 + 2 + 3).
+
+Example: 6, 28, 496, 8128, 33_550_336, 8_589_869_056
+
+Problem: Print out perfect numbers up to given number: 10,000
+ */
+
+package main.java;
+
+public class PerfectNumbers {
+
+    public static void main(String[] args) {
+
+
+    }
+}
