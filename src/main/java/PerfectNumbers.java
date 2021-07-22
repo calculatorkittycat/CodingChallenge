c4e809136b8d5e52d7b5c9467fc4e7a88ad086df
Oleg Iakim <execute067@gmail.com>
2021-07-22 16:19:05 -0700

    added some work

--- src/main/java/PerfectNumbers.java
+++ src/main/java/PerfectNumbers.java
@@ -12,8 +12,15 @@ package main.java;
 
 public class PerfectNumbers {
 
-    public static void main(String[] args) {
-
-
+        public static boolean isPerfect(int number){
+            int temp = 0;
+            for (int i = 1; i <= number / 2; i++) {if (number % i == 0) {temp += i;}}
+            if (temp == number) {return true;}
+            else {return false;}
+        }
+        public static void main(String[] args) {
+            int max = 10_000;
+            for (int i = 1; i <= max; i++) {if (isPerfect(i)) {System.out.print(i + ", ");}}
+        }
     }
-}
+
