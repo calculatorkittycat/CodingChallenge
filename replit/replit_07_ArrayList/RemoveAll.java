cd856b2a284548be5418c19476df14ba7271dfb0
Oleg Iakim <execute067@gmail.com>
2021-07-23 10:47:19 -0700

    Task RemoveAll Complete

---
+++ replit/replit_07_ArrayList/RemoveAll.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class RemoveAll {
+
+    //create your method below
+
+    public static ArrayList<String> removeAll(ArrayList<String> wordList, String targetWorld){
+
+        wordList.removeAll(Arrays.asList(targetWorld));
+
+        return wordList;
+
+    }
+
+    // Do not touch below
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int size = in.nextInt();
+        String target = in.next();
+        ArrayList<String> list = new ArrayList<>();
+        if (size == 7){
+            for(int i=0; i < size - 1; i++) {
+                list.add(in.next());
+            }
+        }else{
+            for(int i=0; i < size; i++) {
+                list.add(in.next());
+            }
+        }
+
+        System.out.println(removeAll(list, target));
+
+    }
+
+
+
+}
